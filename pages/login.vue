381ad9199cc4df6e9b9a5479e590ddefb23c628f
mgarmao <mgarmao830@gmail.com>
2023-07-26 08:51:10 -0400

    Adding new body css login

--- pages/login.vue
+++ pages/login.vue
@@ -17,10 +17,13 @@
 </script>
 
 <style scoped>
-    /* body{
+    body{
         transition: all ease 0.5s;
         background-color: #121212;
-    } */
+        color: #FFF;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    
     h1{
         color: #fff;
         font-family: Arial, Helvetica, sans-serif;
