c81215884ef9d30b65fb6ab3973eb915cfe62387
mgarmao <mgarmao830@gmail.com>
2023-10-24 14:08:53 -0400

    start of user path  verificatio

--- app.vue
+++ app.vue
@@ -17,13 +17,34 @@ import { useRouter, useRoute } from 'vue-router'
 
   const hide = ref(false)
 
+  const authNeededPages = ["budget","tracker","charts"]
+
   const chartsFillColor = ref("#fff")
   const trackerFillColor = ref("#fff")
   const budgetFillColor = ref('#fff')
 
   router.beforeEach (async(to, from)=>{
+    console.log("run")
     changeColors(to.fullPath)
   })
+
+  const checkUserStatus = (path)=>{
+    const userSignedIn = isUserSignedIn()
+    if(userSignedIn){
+      if(path=="login"||path=="signup"){
+        console.log("Redirect Home")
+      }
+    }
+
+    else{
+      for(let i=0;i<authNeededPages.length; i++){
+        console.log(authNeededPages[i])
+      }
+    }
+  }
+
+  checkUserStatus(route)
+
 
   const changeColors = (path)=>{
     if((path=="/login")||(path=="/signup")||(path=="/")){
